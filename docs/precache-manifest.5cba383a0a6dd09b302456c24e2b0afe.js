24a456d2600a39319bfd969419cfc150c9f19237
Victor E Gonzalez P <vemiliogp@gmail.com>
2020-06-20 01:02:18 -0400

    Docs agregado

---
+++ docs/precache-manifest.5cba383a0a6dd09b302456c24e2b0afe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3418877b2a7b779993a64dd8641050ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb840940da50e7d9fc72",
+    "url": "/static/css/main.0a5a2069.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb840940da50e7d9fc72",
+    "url": "/static/js/main.f7f64e62.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
